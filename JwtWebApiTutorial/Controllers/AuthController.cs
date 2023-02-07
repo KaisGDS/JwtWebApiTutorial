09fbbdf9d3002e33c906ecad76d26224e3f2e3eb
Kaissar <k.shalabayev@gmail.com>
2023-02-07 13:52:54 -0600

    Add project files.

---
+++ JwtWebApiTutorial/Controllers/AuthController.cs
@@ -0,0 +1,102 @@
+﻿using JwtWebApiTutorial.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace JwtWebApiTutorial.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+        private readonly IUserService _userService;
+
+        public AuthController(IConfiguration configuration, IUserService userService) 
+        { 
+            _configuration= configuration;
+            _userService= userService;
+        }
+
+        [HttpGet, Authorize]
+        public ActionResult<string> GetInfo()
+        {   
+            var username =  _userService.GetUsername();
+            return Ok(username);
+
+            //var username2 = User.FindFirstValue(ClaimTypes.Name);
+            //var role = User.FindFirstValue(ClaimTypes.Role);
+            //return Ok(new { username, username2, role});
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Username= request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found.");
+            }
+
+            if (!VerifyPasswordHAsh(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddDays(1), signingCredentials: cred);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHAsh(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
---
+++ JwtWebApiTutorial/Services/UserService/UserService.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+
+namespace JwtWebApiTutorial.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        public UserService(IHttpContextAccessor httpContextAccessor) { 
+            _contextAccessor = httpContextAccessor;
+        }
+        public string GetUsername()
+        {
+            var result = string.Empty;
+            if (_contextAccessor.HttpContext!=null)
+            {
+                result = _contextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+            }
+            return result;
+        }
+    }
+}
